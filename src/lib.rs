5084e3090653ea38493f2c55a9f6204c2bf03196
Geoffroy Couprie <geo.couprie@gmail.com>
2017-03-20 18:43:01 +0100

    readme and other informations

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 //! Cookie factory
+//! # cookie-factory
+//!
+//! serialization library built on the same design as nom.
+//!
+//! Highly experimental, don't use it if you're afraid of rewriting all your code
 #![cfg_attr(feature = "nightly", feature(trace_macros))]
 
 pub use gen::*;
