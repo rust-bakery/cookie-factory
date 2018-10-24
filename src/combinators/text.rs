faf5d71482a5c4afc75205c4e54aa2322336f947
Geoffroy Couprie <geo.couprie@gmail.com>
2018-10-24 16:19:37 +0200

    reuse std::fmt::Display if possible

--- src/combinators/text.rs
+++ src/combinators/text.rs
@@ -1,5 +1,9 @@
 use std::str;
+use std::fmt;
+use std::io::{Write,Cursor};
+use super::{Serializer, Serialized};
 use super::binary::Slice;
+use gen::GenError;
 
 pub trait StrSr {
   fn raw<'a>(&'a self) -> Slice<'a>;
@@ -12,10 +16,37 @@ impl<S: AsRef<str>> StrSr for S {
   }
 }
 
+pub struct Print<'a, T> {
+  value: &'a T,
+}
+
+impl<'a, T: fmt::Display> Serializer for Print<'a, T> {
+  fn serialize<'b, 'c>(&'b mut self, output: &'c mut [u8]) -> Result<(usize, Serialized), GenError> {
+    let mut c = Cursor::new(output);
+    match write!(&mut c, "{}", self.value) {
+      //FIXME: maybe return an error here instead of assuming the buffer is too small?
+      Err(_) => Ok((0, Serialized::Continue)),
+      Ok(_) => Ok((c.position() as usize, Serialized::Done))
+    }
+  }
+}
+
+pub trait PrintSr<'a>: Sized {
+  fn print(&'a self) -> Print<'a, Self>;
+}
+
+impl<'a, T: fmt::Display> PrintSr<'a> for T {
+  #[inline(always)]
+  fn print(&'a self) -> Print<'a, T> {
+    Print { value: self }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
   use super::super::{Serializer, Serialized};
+  use std::str::from_utf8;
 
   #[test]
   fn str_serializer() {
@@ -30,6 +61,29 @@ mod tests {
 
     assert_eq!(sr.serialize(s), Ok((6, Serialized::Done)));
     assert_eq!(&s[..], b"world!");
+  }
+
+  #[test]
+  fn print_serializer() {
+    let mut mem: [u8; 100] = [0; 100];
+    let s = &mut mem[..];
+
+    let string = "hello";
+    assert_eq!(string.print().serialize(s), Ok((5, Serialized::Done)));
+    assert_eq!(from_utf8(&s[..5]).unwrap(), "hello");
+
+    let num = 1234;
+    assert_eq!(num.print().serialize(s), Ok((4, Serialized::Done)));
+    assert_eq!(from_utf8(&s[..4]).unwrap(), "1234");
+
+    let f = -1.2e14f32;
+    assert_eq!(f.print().serialize(s), Ok((16, Serialized::Done)));
+    assert_eq!(from_utf8(&s[..16]).unwrap(), "-120000000000000");
+
+    let f2 = 1234.56e-2f64;
+    assert_eq!(f2.print().serialize(s), Ok((7, Serialized::Done)));
+    assert_eq!(from_utf8(&s[..7]).unwrap(), "12.3456");
+
   }
 
 }
