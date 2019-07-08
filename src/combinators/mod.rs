bc62eda43be04ac593cff5ae286bf6a39e95c6b7
Sebastian Dröge <sebastian@centricular.com>
2019-07-08 23:31:57 +0300

    Add tuple combinator

--- src/combinators/mod.rs
+++ src/combinators/mod.rs
@@ -810,6 +810,120 @@ where
     }
 }
 
+/// Helper trait for the `tuple` combinator
+pub trait Tuple<W> {
+    fn serialize(&self, w: W) -> GenResult<W>;
+}
+
+// Generates all the Tuple impls for tuples of arbitrary sizes based on a list of type
+// parameters like FnA FnB FnC. It would generate the impl then for (FnA, FnB)
+// and (FnA, FnB, FnC).
+macro_rules! tuple_trait(
+  ($name1:ident, $name2: ident, $($name:ident),*) => (
+    tuple_trait!(__impl $name1, $name2; $($name),*);
+  );
+  (__impl $($name:ident),+; $name1:ident, $($name2:ident),*) => (
+    tuple_trait_impl!($($name),+);
+    tuple_trait!(__impl $($name),+ , $name1; $($name2),*);
+  );
+  (__impl $($name:ident),+; $name1:ident) => (
+    tuple_trait_impl!($($name),+);
+    tuple_trait_impl!($($name),+, $name1);
+  );
+);
+
+// Generates the impl block for Tuple on tuples or arbitrary sizes based on its
+// arguments. Takes a list of type parameters as parameters, e.g. FnA FnB FnC
+// and then implements the trait on (FnA, FnB, FnC).
+macro_rules! tuple_trait_impl(
+  ($($name:ident),+) => (
+    impl<W: Write, $($name: SerializeFn<W>),+> Tuple<W> for ( $($name),+ ) {
+      fn serialize(&self, w: W) -> GenResult<W> {
+        tuple_trait_inner!(0, self, w, $($name)+)
+      }
+    }
+  );
+);
+
+// Generates the inner part of the Tuple::serialize() implementation, which will
+// basically look as follows:
+//
+// let w = self.0(w)?;
+// let w = self.1(w)?;
+// [...]
+// let w = self.N(w)?;
+//
+// Ok(w)
+macro_rules! tuple_trait_inner(
+  ($it:tt, $self:expr, $w:ident, $head:ident $($id:ident)+) => ({
+    let w = $self.$it($w)?;
+
+    succ!($it, tuple_trait_inner!($self, w, $($id)+))
+  });
+  ($it:tt, $self:expr, $w:ident, $head:ident) => ({
+    let w = $self.$it($w)?;
+
+    Ok(w)
+  });
+);
+
+// Takes an integer and a macro invocation, and changes the macro invocation
+// to take the incremented integer as the first argument
+//
+// Works for integers between 0 and 19.
+#[doc(hidden)]
+macro_rules! succ (
+  (0, $submac:ident ! ($($rest:tt)*)) => ($submac!(1, $($rest)*));
+  (1, $submac:ident ! ($($rest:tt)*)) => ($submac!(2, $($rest)*));
+  (2, $submac:ident ! ($($rest:tt)*)) => ($submac!(3, $($rest)*));
+  (3, $submac:ident ! ($($rest:tt)*)) => ($submac!(4, $($rest)*));
+  (4, $submac:ident ! ($($rest:tt)*)) => ($submac!(5, $($rest)*));
+  (5, $submac:ident ! ($($rest:tt)*)) => ($submac!(6, $($rest)*));
+  (6, $submac:ident ! ($($rest:tt)*)) => ($submac!(7, $($rest)*));
+  (7, $submac:ident ! ($($rest:tt)*)) => ($submac!(8, $($rest)*));
+  (8, $submac:ident ! ($($rest:tt)*)) => ($submac!(9, $($rest)*));
+  (9, $submac:ident ! ($($rest:tt)*)) => ($submac!(10, $($rest)*));
+  (10, $submac:ident ! ($($rest:tt)*)) => ($submac!(11, $($rest)*));
+  (11, $submac:ident ! ($($rest:tt)*)) => ($submac!(12, $($rest)*));
+  (12, $submac:ident ! ($($rest:tt)*)) => ($submac!(13, $($rest)*));
+  (13, $submac:ident ! ($($rest:tt)*)) => ($submac!(14, $($rest)*));
+  (14, $submac:ident ! ($($rest:tt)*)) => ($submac!(15, $($rest)*));
+  (15, $submac:ident ! ($($rest:tt)*)) => ($submac!(16, $($rest)*));
+  (16, $submac:ident ! ($($rest:tt)*)) => ($submac!(17, $($rest)*));
+  (17, $submac:ident ! ($($rest:tt)*)) => ($submac!(18, $($rest)*));
+  (18, $submac:ident ! ($($rest:tt)*)) => ($submac!(19, $($rest)*));
+  (19, $submac:ident ! ($($rest:tt)*)) => ($submac!(20, $($rest)*));
+);
+
+tuple_trait!(FnA, FnB, FnC, FnD, FnE, FnF, FnG, FnH, FnI, FnJ, FnK, FnL,
+FnM, FnN, FnO, FnP, FnQ, FnR, FnS, FnT, FnU);
+
+/// Applies multiple serializers in sequence
+///
+/// Currently tuples up to 20 elements are supported.
+///
+/// ```rust
+/// use cookie_factory::{tuple, string, be_u16};
+///
+/// let mut buf = [0u8; 100];
+///
+/// {
+///   let buf = tuple((
+///     string("abcd"),
+///     be_u16(0x20),
+///     string("efgh"),
+///   ))(&mut buf[..]).unwrap();
+///   assert_eq!(buf.len(), 100 - 10);
+/// }
+///
+/// assert_eq!(&buf[..10], &b"abcd\x00\x20efgh"[..]);
+/// ```
+pub fn tuple<W: Write, List: Tuple<W>>(l: List) -> impl SerializeFn<W> {
+    move |w: W| {
+        l.serialize(w)
+    }
+}
+
 //missing combinators:
 //or
 //empty
@@ -864,6 +978,27 @@ mod test {
         }
 
         assert_eq!(&buf[..], b"12345678");
+    }
+
+    #[test]
+    fn test_tuple() {
+        let mut buf = [0u8; 12];
+
+        {
+            use self::io::Cursor;
+
+            let mut cursor = Cursor::new(&mut buf[..]);
+            let serializer = tuple((
+                string("1234"),
+                string("5678"),
+                string("0123"),
+            ));
+
+            let cursor = serializer(cursor).unwrap();
+            assert_eq!(cursor.position(), 12);
+        }
+
+        assert_eq!(&buf[..], b"123456780123");
     }
 
     #[test]
